08ff7a7a97cf61870f90200e72d5701fea118669
laxmanc4c <laxmanc4c@gmail.com>
2024-07-04 12:49:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,24 +17,40 @@ document.addEventListener('DOMContentLoaded', function() {
         pincode: document.getElementById('pincode').value
       };
      
-    gigya.cdp.init({
-        apiDomain: 'EU5',
-        bUnitId: '4_2arKfv5bsPsK9ODVBhCJeA',
-        appId: 'HHDD-XdWAy3F82dmfNhegA'
-    })
-    .then(function(sdk) { return window.CDP = sdk;
-  CDP.report('Registration_Form',
+gigya.cdp.init({
+
+      apiDomain: 'EU5',
+
+      bUnitId: '4_vuyHuRd8K_y9KrWOKNHd0A',
+
+      appId: 'HIZ_ZYqCQQrOAywJZwT7Bg'
+
+  })
+
+  .then(function(sdk) { window.CDP = sdk;
+
+      CDP.report('Registration_Form',
+
           {
+
               "emailid": email,
+
               "firstname": firstName
+
           }
+
           );
+
           alert('Form submitted successfully!');
+
    }).catch(function(error) {
+
       console.error('CDP initialization error:', error);
+
       alert("Error reporting data to CDP.");
-                        
-});
+
+  });
+ 
      
       this.reset();
     });
